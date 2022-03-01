1458f4a3471767db65f1d066f287a4268e7568fe
Didier <blessingmosley@hotmail.com>
2022-03-01 00:55:37 -0500

    Some changes made, but bugs in the code

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,16 +1,57 @@
 import '../App.css';
 import React, { useRef } from 'react';
-import { ReactDOM } from 'react';
+// import ReactDOM from 'react-dom';
 
 export default function Content(){
-    const inputArea = useRef();
+    const inputArea = useRef('');
     const editable = useRef(null);
 
     const createElements = ()=>{
+        /*const taskName = React.createElement('span',{},'Item');
+        const btnContainer = React.createElement('div',{className:'btn'},[
+            React.createElement('ion-icon',{name:'create'},''),
+            React.createElement('ion-icon',{name:'trash'},'')
+        ])
+        const tasks = React.createElement('div',{className:'item'},[taskName,btnContainer]);
+
+        ReactDOM.render(tasks,document.getElementById('items'))
         
-        const tasks = React.createElement('div',{},'Prueba')
+        console.log(inputArea.current.value);*/
+
+        const tasks = document.createElement("DIV");
+        tasks.classList.add('item');
+
+        const taskName = document.createElement("SPAN");
+        taskName.setAttribute("ref",editable);
+        let name = inputArea.current.value;
         console.log(inputArea.current.value);
+        if(inputArea.current.value===''){
+            return false;
+        }else{
+            taskName.textContent = name;
+            inputArea.current.value='';
+        }
+        
+        const edit = ()=>{
+            return editable.current.setAttribute("contenteditable","true");
+        }
+        const btnContainer = document.createElement("DIV");
+        btnContainer.classList.add('btn');
+
+        const create = document.createElement("ion-icon");
+        create.setAttribute("name","create");
+        create.setAttribute("onClick", edit());
+
+        const trash = document.createElement("ion-icon");
+        trash.setAttribute("name","trash"); 
+
+        btnContainer.appendChild(create);
+        btnContainer.appendChild(trash);
+        tasks.appendChild(taskName);
+        tasks.appendChild(btnContainer);
+        document.getElementById('items').appendChild(tasks);
     }
+
 
     return(
         <>
@@ -21,14 +62,15 @@ export default function Content(){
             <div className="card">
                 <h2 className="title">Title</h2>
                 <div className="items" id='items'>
-                    <div className="item">
+                    {/* <div className="item">
                         <span>Item</span>
                         <div className="btn">
                             <ion-icon name="create"></ion-icon>
                             <ion-icon name="trash"></ion-icon>
                         </div>
-                    </div> 
+                    </div>  */}
 
+                </div>
                     <div className='btn submit'>
                         <input placeholder='Type your task here' ref={inputArea}/> 
 
@@ -37,7 +79,6 @@ export default function Content(){
                         </div>
                         
                     </div>      
-                </div>
             </div>
         </div>
         
